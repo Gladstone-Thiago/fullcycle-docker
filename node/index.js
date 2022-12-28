4ef08b9311c1722d3fdca5df0605236f7e7d6d60
Gladstone <thiagogladstone@Gladstones-MacBook-Pro.local>
2022-12-28 00:09:56 -0300

    added mysql compose and node app

--- node/index.js
+++ node/index.js
@@ -1,11 +1,26 @@
-const express = require("express")
+
+
+const express = require('express')
 const app = express()
 const port = 3000
+const config = {
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database:'nodedb'
+};
+const mysql = require('mysql')
+const connection = mysql.createConnection(config)
 
-app.get('/', (req, res) => {
-    res.send('<h1>Full cycle</h1>')
+const sql = `INSERT INTO people(name) values('Wesley')`
+connection.query(sql)
+connection.end()
+
+
+app.get('/', (req,res) => {
+    res.send('<h1>Full Cycle</h1>')
 })
 
 app.listen(port, ()=> {
-    console.log('Running in port ' + port)
+    console.log('Running port' + port)
 })
